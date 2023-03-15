c6abfec674285d583340ff80173e61d7e520e58f
VaidasiganLorena <92209059+VaidasiganLorena@users.noreply.github.com>
2023-03-15 18:20:54 +0200

    Update

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import express from 'express'
-import { Router } from 'express'
 import mysql from 'mysql2'
 import bcrypt from 'bcryptjs'
 import bodyParser from 'body-parser'
 import { body, validationResult } from 'express-validator'
+import cors from 'cors'
+import jwt from 'jsonwebtoken'
 
 const app = express()
-
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  }),
+)
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -17,68 +24,124 @@ const db = mysql.createConnection({
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/user/login', (req, res) => {
+const generateAccessToken = (email) => {
+  return jwt.sign({ email }, `${process.env.SECRET_TOKEN}`, {
+    expiresIn: '3600s',
+  })
+}
+
+app.get('/user', (req, res) => {
   const bank_of_time = db
   bank_of_time.execute(`SELECT * FROM users `, (dbErr, dbRes) => {
     if (dbErr) {
       res.status(400).send({ response: dbErr.message, status: 400 }).end()
-      bank_of_time.end()
-      return
     }
     if (dbRes) {
       res.status(200).send({ response: dbRes, status: 200 })
-      bank_of_time
-      return
     }
   })
-}),
-  app.post(
-    '/user/register',
-    body('firstName').isLength({ min: 1 }),
-    body('lastName').isLength({ min: 1 }),
-    body('email').isEmail().normalizeEmail(),
-    body('phoneNumber').isLength({ min: 1 }),
-    body('password').isLength({ min: 1 }),
-    body('city').isLength({ min: 1 }),
-    body('gender').isLength({ min: 1 }),
-    (req, res) => {
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return res.status(400).send(errors).end()
-      } else {
-        const userInformation = {
-          firstname: req.body.firstName,
-          lastname: req.body.lastName,
-          email: req.body.email,
-          phoneNumber: req.body.phoneNumber,
-          password: req.body.password,
-          city: req.body.city,
-          gender: req.body.gender,
-        }
+})
+app.post(
+  '/user/login',
+  body('email').isEmail().normalizeEmail(),
+  body('password').isLength({ min: 1 }),
+  (req, res) => {
+    const errors = validationResult(req)
 
-        const bank_of_time = db
-        const hashedPw = bcrypt.hashSync(userInformation.password, 10)
-        if (hashedPw) {
-          bank_of_time.execute(
-            `INSERT INTO users (firstname, lastname, email, phoneNumber, password, city, gender, userUuid) VALUES (
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors).end()
+    } else {
+      const userLoginInfo = {
+        email: req.body.email,
+        password: req.body.password,
+      }
+      const bank_of_time = db
+      bank_of_time.execute(
+        `SELECT userUuid, email, password FROM users WHERE email = '${userLoginInfo.email}'`,
+        (dbErr, dbRes) => {
+          if (dbErr) {
+            res.status(401).send({ response: dbErr.message, status: 401 }).end()
+          }
+          if (dbRes) {
+            if (dbRes.length === 0) {
+              res
+                .status(401)
+                .send({ response: 'Acest email nu este asociat cu nici un cont.', status: 401 })
+                .end()
+
+              return
+            } else {
+              if (bcrypt.compareSync(userLoginInfo.password, dbRes[0].password)) {
+                const authToken = generateAccessToken(dbRes[0].email)
+
+                res
+                  .status(200)
+                  .send({
+                    response: {
+                      userUuid: dbRes[0].userUuid,
+                      authToken: authToken,
+                    },
+                    status: 200,
+                  })
+                  .end()
+              } else {
+                res
+                  .status(401)
+                  .send({ response: 'Emailul & parola sunt greșite!', status: 401 })
+                  .end()
+              }
+            }
+          }
+        },
+      )
+    }
+  },
+)
+app.post(
+  '/user/register',
+  body('firstName').isLength({ min: 1 }),
+  body('lastName').isLength({ min: 1 }),
+  body('email').isEmail().normalizeEmail(),
+  body('phoneNumber').isLength({ min: 1 }),
+  body('password').isLength({ min: 1 }),
+  body('city').isLength({ min: 1 }),
+  body('gender').isLength({ min: 1 }),
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors).end()
+    } else {
+      const userInformation = {
+        firstname: req.body.firstName,
+        lastname: req.body.lastName,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        password: req.body.password,
+        city: req.body.city,
+        gender: req.body.gender,
+      }
+
+      console.log(userInformation)
+
+      const bank_of_time = db
+      const hashedPw = bcrypt.hashSync(userInformation.password, 10)
+      if (hashedPw) {
+        bank_of_time.execute(
+          `INSERT INTO users (firstname, lastname, email, phoneNumber, password, city, gender, userUuid) VALUES (
       '${userInformation.firstname}','${userInformation.lastname}','${userInformation.email}' ,'${userInformation.phoneNumber}', '${hashedPw}' ,
       '${userInformation.city}','${userInformation.gender}', uuid());`,
-            (dbErr, dbRes) => {
-              if (dbErr) {
-                res.status(400).send({ response: dbErr.message, status: 400 }).end()
-                bank_of_time.end()
-                return
-              }
-              if (dbRes) {
-                console.log(dbRes)
-                res.status(200).send({ response: 'User created with success!', status: 200 }).end()
-                bank_of_time
-                return
-              }
-            },
-          )
-        }
+          (dbErr, dbRes) => {
+            if (dbErr) {
+              res.status(400).send({ response: dbErr.message, status: 400 }).end()
+            }
+            if (dbRes) {
+              console.log(dbRes)
+              res.status(200).send({ response: 'User created with success!', status: 200 }).end()
+            }
+          },
+        )
       }
-    },
-  ),
+    }
+  },
+),
   app.listen('3306')
